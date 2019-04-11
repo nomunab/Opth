13522199ec9fcca293f5082724e8b3f581be3da6
Itzel Hernandez <inhernandez@ucdavis.edu>
2019-04-10 17:41:43 -0700

    filtering works, but functionality not fully yet

--- Opth/View Controllers/ContentsOfTableViewController.swift
+++ Opth/View Controllers/ContentsOfTableViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import Foundation
 
-//TODO: add category
 //populate the table
 struct cellData {
     var opened = Bool()
@@ -18,14 +17,28 @@ struct cellData {
     var subtopic: [String]
 }
 
-class ContentsOfTableViewController: UITableViewController {
+// in order to have them opened one by one appropriately
+enum selectedScope: Int {
+    case category = 0
+    case topic = 1
+    case subtopic = 2
+}
+
+class ContentsOfTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var tableViewData = [cellData]()
     var topic:Topic = Topic(topic: "null")
     
+    //for searching
+    var filteredData = [cellData]()
+    var isSearching = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        parse.csv(data:"/Users/cathyhsieh/Documents/GitHub/Opth/Opth/Information/biggerdata.txt")
+        parse.csv(data:"/Users/Itzel/Desktop/Opth/Opth/Information/biggerdata.txt")
+        //parse.csv(data:"/Users/cathyhsieh/Documents/GitHub/Opth/Opth/Information/biggerdata.txt")
         
         // use for loop to parse through all the categories,topics, and subtopic
         let categories = status.CategoryList
@@ -74,16 +87,25 @@ class ContentsOfTableViewController: UITableViewController {
                 tableViewData.append(cellData(opened: false, category: [categoryKey], topic: [topicKey], subtopic: topicValue))
             }
         }
+        
+        // for searching
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.default
     }
     
+    // MARK: - table of contents
     override func numberOfSections(in tableView: UITableView) -> Int {
         return tableViewData.count
     }
     
     //this one seems fine
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //searching
+        if isSearching {
+            return filteredData.count
+        }
         // shows subtopics when topic is clicked
-        if tableViewData[section].opened == true {
+        else if (tableViewData[section].opened == true) {
             return tableViewData[section].subtopic.count + 1
         }
         else {
@@ -95,6 +117,66 @@ class ContentsOfTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let newIndex = indexPath.row
         let dataIndex = indexPath.row - 1
+        
+        // this seems to be the problem
+        if isSearching {
+            switch indexPath.row{
+            case 0:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                    return UITableViewCell()}
+                cell.textLabel?.text = filteredData[indexPath.section].category[newIndex]
+                cell.textLabel?.textColor = UIColor.white
+                return cell
+            case 1:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                    return UITableViewCell()}
+                cell.textLabel?.text = filteredData[indexPath.section].topic[dataIndex]
+                cell.textLabel?.textColor = UIColor.white
+                return cell
+            case 2:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                    return UITableViewCell()}
+                cell.textLabel?.text = filteredData[indexPath.section].subtopic[dataIndex]
+                cell.textLabel?.textColor = UIColor.white
+                return cell
+            default:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                    return UITableViewCell()}
+                cell.textLabel?.text = filteredData[indexPath.section].subtopic[dataIndex]
+                cell.textLabel?.textColor = UIColor.white
+                return cell
+            }
+        }
+        else {
+        // make switch statement work properly
+            switch indexPath.row{
+            case 0:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                    return UITableViewCell()}
+                cell.textLabel?.text = tableViewData[indexPath.section].category[newIndex]
+                cell.textLabel?.textColor = UIColor.white
+                return cell
+            case 1:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                    return UITableViewCell()}
+                cell.textLabel?.text = tableViewData[indexPath.section].topic[dataIndex]
+                cell.textLabel?.textColor = UIColor.white
+                return cell
+            case 2:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                    return UITableViewCell()}
+                cell.textLabel?.text = tableViewData[indexPath.section].subtopic[dataIndex]
+                cell.textLabel?.textColor = UIColor.white
+                return cell
+            default:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                    return UITableViewCell()}
+                cell.textLabel?.text = tableViewData[indexPath.section].subtopic[dataIndex]
+                cell.textLabel?.textColor = UIColor.white
+                return cell
+            }
+        }
+        /*
         if indexPath.row == 0 {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
                 return UITableViewCell()}
@@ -116,7 +198,7 @@ class ContentsOfTableViewController: UITableViewController {
             cell.textLabel?.text = tableViewData[indexPath.section].subtopic[dataIndex]
             cell.textLabel?.textColor = UIColor.white
             return cell
-        }
+        }*/
     }
     
     // write within this function in order to make subtopic redirect to card
@@ -136,5 +218,18 @@ class ContentsOfTableViewController: UITableViewController {
         }
     }
     
+    // this is working
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText:String) {
+            isSearching = true
+            filteredData = tableViewData.filter({$0.category.joined(separator: ",").lowercased().prefix(searchText.count) == searchText.lowercased()})
+            filteredData = tableViewData.filter({$0.topic.joined(separator: ",").lowercased().prefix(searchText.count) == searchText.lowercased()})
+            filteredData = tableViewData.filter({$0.subtopic.joined(separator: ",").lowercased().prefix(searchText.count) == searchText.lowercased()})
+            tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
+        searchBar.text = ""
+        tableView.reloadData()
+    }
 }
-
